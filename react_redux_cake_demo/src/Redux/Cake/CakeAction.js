0c0f553547190e24b99681d97193e4ed8a733c9f
chikkugeorge24 <chikkugeorge24@gmail.com>
2020-02-27 22:53:13 +0530

    v1.0 react-redux-demo cake shop action types and creators commit

---
+++ react_redux_cake_demo/src/Redux/Cake/CakeAction.js
@@ -0,0 +1,9 @@
+import { BUY_CAKE } from './CakeTypes';
+const buy_cake = () => {
+    return {
+        type: BUY_CAKE
+    }
+}
+export {
+    buy_cake
+}
---
+++ react_redux_cake_demo/src/Redux/Cake/CakeTypes.js
@@ -0,0 +1,4 @@
+const BUY_CAKE = `BUY_CAKE`;
+export {
+    BUY_CAKE
+}
