69219a821579a5ca4c869b0f24a1b5f12c120537
chikkugeorge24 <chikkugeorge24@gmail.com>
2020-02-27 22:36:53 +0530

    v1.0 react-redux-demo cake shop components commit

---
+++ react_redux_cake_demo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CakeContainer from './components/CakeContainer';
+
+function App() {
+  return (
+    <React.Fragment>
+        <CakeContainer/>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ react_redux_cake_demo/src/components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class CakeContainer extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h2> Number of Cakes</h2>
+                <button> BUY CAKE </button>
+            </div>
+        );
+    }
+}
+export default CakeContainer;
