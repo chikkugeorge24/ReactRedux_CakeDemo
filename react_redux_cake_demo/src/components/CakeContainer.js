56259f7dadb3aae72f511526aed6b5d56269f9ba
chikkugeorge24 <chikkugeorge24@gmail.com>
2020-03-02 23:12:45 +0530

    v1.0 react-redux-demo connecting redux store commit

--- react_redux_cake_demo/src/components/CakeContainer.js
+++ react_redux_cake_demo/src/components/CakeContainer.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { buy_cake } from '../Redux/Cake/CakeAction';
 class CakeContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
+        const { numOfCakes, buyCake } = this.props;
+        console.log(numOfCakes)
         return (
             <div className="App">
-                <h2> Number of Cakes</h2>
-                <button> BUY CAKE </button>
+                <h2> Number of Cakes -- {numOfCakes}</h2>
+                <button onClick={buyCake}> BUY CAKE </button>
             </div>
         );
     }
 }
-export default CakeContainer;
+
+const mapStateToProps = (state) => {
+    return {
+        numOfCakes: state.numOfCakes
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        buyCake: () => dispatch(buy_cake())
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(CakeContainer);
